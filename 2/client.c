10d43e75169454df645b4b5d03766f491d236e4d
Shreyas Bhat <shreyasnbhat@gmail.com>
2018-08-18 16:21:23 +0530

    Added documentation in Assignment 2

--- 2/client.c
+++ 2/client.c
@@ -26,6 +26,13 @@ int main(int argc, char const *argv[]) {
   }
 
   port = atoi(argv[1]);
+
+  /* Creation of the socket
+  * socketfd is Socket File Descriptor Entry
+  * AF_INET is used to specify the address family which is IPv4
+  * SOCK_STREAM is used to specify a connection based protocol i.e TCP
+  * 0 specifies the protocol value for IP, which is visible in the IP Header in the protocol field
+  */
   socketfd = socket(AF_INET,SOCK_STREAM,0);
   if(socketfd < 0) {
     error("Error opening socket!");
@@ -42,16 +49,32 @@ int main(int argc, char const *argv[]) {
   bcopy((char *)server->h_addr,(char*)&server_addr.sin_addr.s_addr,server->h_length);
   server_addr.sin_port = htons(port);
 
+  /* Connects the socket referred to by the file descriptor socketfd to the address specified by server_addr
+  * socketfd is Socket File Descriptor Entry
+  * server_addr is the pointer to a sockaddr_in structure containing the address to connect
+  * The third arguement is the length of this address
+  */
   if(connect(socketfd,&server_addr,sizeof(server_addr)) < 0)
     error("Error connecting to host!");
 
   printf("What is your name? ");
   bzero(buffer,256);
   fgets(buffer,255,stdin);
+
+  /* Write to socket
+  * socketfd is the socket descriptor
+  * buffer is the pointer to the data to be written
+  * strlen(buffer) is the length of the data to be written
+  */
   n = write(socketfd,buffer,strlen(buffer));
   if(n < 0) error("Error writing to socket!");
   bzero(buffer,256);
 
+  /* Read from socket buffer
+  * socketfd is the socket descriptor
+  * buffer is the pointer to the socket buffer where all socket data is present
+  * 255 is the socket buffer size
+  */
   n = read(socketfd,buffer,255);
   if(n < 0) error("Error reading from socket!");
   printf("%s\n",buffer);
--- 2/server.c
+++ 2/server.c
@@ -20,7 +20,11 @@ int main(int argc, char const *argv[]) {
     char buffer[256];
     struct sockaddr_in server_addr,client_addr;
 
-    // socketfd is Socket File Descriptor Entry
+    /* socketfd is Socket File Descriptor Entry
+    * AF_INET is used to specify the address family which is IPv4
+    * SOCK_STREAM is used to specify a connection based protocol i.e TCP
+    * 0 specifies the protocol value for IP, which is visible in the IP Header in the protocol field
+    */
     int socketfd =  socket(AF_INET,SOCK_STREAM,0);
     if(socketfd < 0) {
       printf("Error opening socket!\n");
@@ -39,23 +43,44 @@ int main(int argc, char const *argv[]) {
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    // bind to socket
+    /* Bind to socket
+    * socketfd is the socket descriptor for the created socket
+    * server_addr is the pointer to a sockaddr structure containing the name that is to be bound to socket
+    * The third arguement is the length of this address
+    */
     if(bind(socketfd, (struct sockaddr*)&server_addr,sizeof(server_addr)) < 0){
       error("Error on binding");
     }
 
-    // Socket listens,5 is the size of the backlog queue
+    /* Socket listens to incoming connections
+    * socketfd is the socket descriptor for the created socket
+    * 5 is the maximum length that the queue of pending connections may grow to. Called as a backlog queue.
+    */
     listen(socketfd,5);
 
     // accept connections
     while(1) {
         int client_length = sizeof(client_addr);
+
+
+        /* Accept connection
+        * socketfd is the socket descriptor for the created socket
+        * client_addr is the pointer to a sockaddr structure containing the name of the connecting entity
+        * client_length is the length of client_addr
+        * acceptfd is the new socket descriptor created for the first connection in the queue
+        */
         int acceptfd = accept(socketfd,(struct sockaddr*)&client_addr,&client_length);
         if(acceptfd < 0){
           error("Error on accept!");
         }
 
         bzero(buffer,256);
+
+        /* Read from socket buffer
+        * acceptd is the socket descriptor for the current connection
+        * buffer is the pointer to the socket buffer where all socket data is present
+        * 255 is the socket buffer size
+        */
         int n = read(acceptfd,buffer,255);
 
         if(n < 0) error("Error reading from socket");
@@ -82,6 +107,12 @@ int main(int argc, char const *argv[]) {
         char message[100];
         buffer[strlen(buffer)- 1] = '!';
         int k=sprintf (message, "%s%s\0", greeting,buffer);
+
+        /* Write to socket
+        * acceptfd is the socket descriptor for the current connection
+        * message is the pointer to the data to be written
+        * strlen(message) is the length of the data to be written
+        */
         n = write(acceptfd,message,strlen(message));
         if(n < 0) error("Error writing to socket");
     }
